57a97cca91df951a94fcb60e3ad5d3502c516f97
佐藤 達也 <tatsuya.sato@ALM0028.local>
2016-08-25 16:45:19 +0900

    指摘された場所の修正。 一番新しいものにフォーカスするようになりました。

--- app/src/main/java/net/appbank/testproject/todoapp/MainActivity.java
+++ app/src/main/java/net/appbank/testproject/todoapp/MainActivity.java
@@ -22,6 +22,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> mAdapter;
@@ -32,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
     //ListView取得
     ListView mListView;
     //取得物入れ
-    String mImte;
+    String mItem;
+    int mLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +53,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //空白じゃなければ
-                if(mEditText.length() != 0) {
+                if(!mEditText.getText().toString().equals("")) {
                     //データ追加
                     addStringData();
+                    //アダプターの数を入れる
+                    mLast = mAdapter.getCount();
+                    //mLast(追加した一番新しいもの)の位置に移動させる
+                    mListView.setSelection(mLast);
                 }
             }
         });
@@ -64,15 +70,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 mListView = (ListView)parent;
-                mImte = (String)mListView.getItemAtPosition(position);
-                mAdapter = (ArrayAdapter<String>)mListView.getAdapter();
-                mAdapter.remove(mImte);
+                mItem = (String)mListView.getItemAtPosition(position);
+                //削除処理
+                mAdapter.remove(mItem);
             }
         });
     }
 
     //更新処理
-    public void addStringData() {
+    private void addStringData() {
         //EditTextのテキストを取得
         mAdapter.add(mEditText.getText().toString());
         //EditTextの中身の削除
