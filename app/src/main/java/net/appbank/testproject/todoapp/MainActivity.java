855c3537ce3b1b0d7f0ce5cd455be34dfa091b2b
佐藤 達也 <tatsuya.sato@ALM0028.local>
2016-08-25 20:43:10 +0900

    カスタマイズListViewの作成

--- app/src/main/java/net/appbank/testproject/todoapp/MainActivity.java
+++ app/src/main/java/net/appbank/testproject/todoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package net.appbank.testproject.todoapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Debug;
 import android.support.design.widget.FloatingActionButton;
@@ -7,9 +8,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,69 +22,84 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import net.appbank.testproject.todoapp.model.Item;
+
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayAdapter<String> mAdapter;
+    List<Item> mList = new ArrayList<>();
     //EditText取得
     EditText mEditText;
     //ボタン取得
     Button mButton;
-    //ListView取得
-    ListView mListView;
-    //取得物入れ
+    // 取得物入れ
     String mItem;
     int mLast;
+    ItemAdapter mAdapter;
+    ListView mListview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         mEditText = (EditText)findViewById(R.id.edittext);
         mButton = (Button)findViewById(R.id.button);
-        mListView = (ListView)findViewById(R.id.listview);
         setSupportActionBar(toolbar);
+        mListview = (ListView)findViewById(R.id.listview);
+        mList.add(new Item("hoge", "hoge", null));
         //ArrayAdapterの生成
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        mAdapter = new ItemAdapter(this, mList);
+        mListview.setAdapter(mAdapter);
         //buttonにクリックイベントを設定
         mButton.setOnClickListener(new View.OnClickListener() {
             //押した時の処理
             @Override
             public void onClick(View view) {
                 //空白じゃなければ
-                if(!mEditText.getText().toString().equals("")) {
+                Log.d("button","hoge");
+                if(mEditText.getText().toString().length() != 0) {
                     //データ追加
+                    Log.d("button","hoge");
                     addStringData();
                     //アダプターの数を入れる
                     mLast = mAdapter.getCount();
                     //mLast(追加した一番新しいもの)の位置に移動させる
-                    mListView.setSelection(mLast);
+//                    mListView.setSelection(mLast);
                 }
             }
         });
-        //Adapterのセット
-        mListView.setAdapter(mAdapter);
-        //List項目のタッチ処理
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                mListView = (ListView)parent;
-                mItem = (String)mListView.getItemAtPosition(position);
-                //削除処理
-                mAdapter.remove(mItem);
-            }
-        });
     }
-
     //更新処理
     private void addStringData() {
         //EditTextのテキストを取得
-        mAdapter.add(mEditText.getText().toString());
+        mList.add(new Item("test", mEditText.getText().toString(), null));
+        mAdapter.notifyDataSetChanged();
         //EditTextの中身の削除
         mEditText.getEditableText().clear();
+    }
+
+    public class ItemAdapter extends ArrayAdapter<Item> {
+        LayoutInflater mInflater;
+
+        public ItemAdapter(Context context, List<Item> list) {
+            super(context, 0, list);
+            this.mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            convertView = mInflater.inflate(R.layout.item_layout, parent, false);
+            TextView nameTextView = (TextView)convertView.findViewById(R.id.name);
+            TextView dateTextView = (TextView)convertView.findViewById(R.id.date);
+            TextView textTextView = (TextView)convertView.findViewById(R.id.text);
+            nameTextView.setText(getItem(position).getName());
+            textTextView.setText(getItem(position).getText());
+            return convertView;
+        }
     }
 }
---
+++ app/src/main/java/net/appbank/testproject/todoapp/model/Item.java
@@ -0,0 +1,27 @@
+package net.appbank.testproject.todoapp.model;
+
+import java.util.Date;
+
+/**
+ * Created by tatsuya.sato on 2016/08/25.
+ */
+public class Item {
+    private String name;
+    private String text;
+    private Date date;
+
+    public Item(String name, String text, Date date) {
+        this.name = name;
+        this.text = text;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
