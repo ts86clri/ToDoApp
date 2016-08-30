a609456f3836979af910e7113aba8afb756b930b
佐藤 達也 <tatsuya.sato@ALM0028.local>
2016-08-30 12:44:04 +0900

    EditActivityに設置したCheckboxからEditTextの背景色を変更できるようになった

--- app/src/main/java/net/appbank/testproject/todoapp/EditActivity.java
+++ app/src/main/java/net/appbank/testproject/todoapp/EditActivity.java
@@ -1,13 +1,19 @@
 package net.appbank.testproject.todoapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by tatsuya.sato on 2016/08/29.
@@ -17,6 +23,14 @@ public class EditActivity extends AppCompatActivity {
     EditText mEditText;
     Button mButton;
     Intent mIntent;
+    CheckBox mCheckBoxRed;
+    CheckBox mCheckBoxBlue;
+    CheckBox mCheckBoxYellow;
+    CheckBox mCheckBoxWhite;
+    private Boolean isCheckWhite;
+    private Boolean isCheckRed;
+    private Boolean isCheckBlue;
+    private Boolean isCheckYellow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +39,59 @@ public class EditActivity extends AppCompatActivity {
         mEditText = (EditText)findViewById(R.id.edittext);
         mButton = (Button) findViewById(R.id.button);
         mIntent = new Intent(EditActivity.this,MainActivity.class);
+        mCheckBoxWhite = (CheckBox)findViewById(R.id.CheckBoxWhite);
+        mCheckBoxRed = (CheckBox)findViewById(R.id.CheckBoxRed);
+        mCheckBoxBlue = (CheckBox)findViewById(R.id.CheckBoxBlue);
+        mCheckBoxYellow = (CheckBox)findViewById(R.id.CheckBoxYellow);
+
+        mCheckBoxWhite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mCheckBoxWhite.isChecked()==true) {
+                    mCheckBoxRed.setChecked(false);
+                    mCheckBoxBlue.setChecked(false);
+                    mCheckBoxYellow.setChecked(false);
+                    mEditText.setBackgroundColor(Color.WHITE);
+                }
+            }
+        });
+        mCheckBoxRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCheckBoxRed.isChecked() == true) {
+                    mCheckBoxWhite.setChecked(false);
+                    mCheckBoxBlue.setChecked(false);
+                    mCheckBoxYellow.setChecked(false);
+                    mEditText.setBackgroundColor(Color.RED);
+                }
+            }
+        });
+        mCheckBoxBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCheckBoxBlue.isChecked() == true) {
+                    mCheckBoxRed.setChecked(false);
+                    mCheckBoxWhite.setChecked(false);
+                    mCheckBoxYellow.setChecked(false);
+                    mEditText.setBackgroundColor(Color.BLUE);
+                }
+            }
+        });
+        mCheckBoxYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCheckBoxYellow.isChecked() == true) {
+                    mCheckBoxRed.setChecked(false);
+                    mCheckBoxBlue.setChecked(false);
+                    mCheckBoxWhite.setChecked(false);
+                    mEditText.setBackgroundColor(Color.YELLOW);
+                }
+            }
+        });
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // startActivity(mIntent);
                 finish();
             }
         });
